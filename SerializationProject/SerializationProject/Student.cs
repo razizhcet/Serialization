c976be3209dec68b731af08a9cba6c8ebaeabdd6
Razi Ahmad <razizhcet@gmail.com>
2018-03-15 19:52:47 +0530

    first commit

---
+++ SerializationProject/SerializationProject/Student.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace SerializationProject
+{
+    [Serializable]
+    class Student
+    {
+        static log4net.ILog log = log4net.LogManager.GetLogger(typeof(Student));
+        int rollNo;
+        string name;
+        string standard;
+        string school;
+        public Student(int rollNo, string name, string standard, string school)
+        {
+            this.rollNo = rollNo;
+            this.name = name;
+            this.standard = standard;
+            this.school = school;
+        }
+        static void Serialize()
+        {
+            FileStream stream = null;
+            try
+            {
+                stream = new FileStream("E:\\BizRun.NET\\student.txt", FileMode.OpenOrCreate);
+                BinaryFormatter formatter = new BinaryFormatter();
+                Student std = new Student(101, "kamil", "tenth", "dps");
+                formatter.Serialize(stream, std);
+            }
+            catch(FileNotFoundException ex) { log.Error(ex.Message); }
+            finally
+            {
+                if (stream != null)
+                    stream.Close();
+            }
+        }
+        static void DeSerialize()
+        {
+            FileStream stream = null;
+            try
+            {
+                stream = new FileStream("E:\\BizRun.NET\\student.txt", FileMode.OpenOrCreate);
+                BinaryFormatter formatter = new BinaryFormatter();
+                Student std = (Student)formatter.Deserialize(stream);
+                log.Info("Roll no. : " + std.rollNo);
+                log.Info("Name : " + std.name);
+                log.Info("Standard : " + std.standard);
+                log.Info("School : " + std.school);
+            }
+            catch (FileNotFoundException ex) { log.Error(ex.Message); }
+            finally
+            {
+                if (stream != null)
+                    stream.Close();
+            }
+        }
+        static void Main(string[] args)
+        {
+            Serialize();
+            DeSerialize();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SerializationProject/SerializationProject/XmlPerson.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace SerializationProject
+{
+    public class Person
+    {
+        [XmlElement("Person")]
+        //[XmlIgnore]
+        public string pName { get; set; }
+        public int pAge { get; set; }
+        public double pWeight { get; set; }
+    }
+    class XmlPerson
+    {
+        static log4net.ILog log = log4net.LogManager.GetLogger(typeof(XmlPerson));
+        static void Serialize()
+        {
+            FileStream file = null;
+            try
+            {
+                Person prsn = new Person();
+                prsn.pName = "Rahul";
+                prsn.pAge = 33;
+                prsn.pWeight = 59.30;
+                XmlSerializer xs = new XmlSerializer(prsn.GetType());
+                file = File.Create("person.xml");
+                xs.Serialize(file, prsn);
+            }
+            catch (FileNotFoundException ex)
+            { Console.WriteLine(ex.Message); }
+            finally
+            {
+                if (file != null)
+                    file.Close();
+            }
+        }
+        static void DeSerialize()
+        {
+            StreamReader file = null;
+            try
+            {
+                XmlSerializer reader = new XmlSerializer(typeof(Person));
+                file = new StreamReader("person.xml");
+                Person prsn = (Person)reader.Deserialize(file);
+                log.Info("Name : " + prsn.pName);
+                log.Info("Age : " + prsn.pAge);
+                log.Info("Weight : " + prsn.pWeight);
+            }
+            catch (FileNotFoundException ex)
+            { Console.WriteLine(ex.Message); }
+            finally
+            {
+                if (file != null)
+                    file.Close();
+            }
+
+
+        }
+        static void Main(string[] args)
+        {
+            Serialize();
+            DeSerialize();
+            Console.WriteLine("done");
+            Console.ReadKey();
+        }
+    }
+}
